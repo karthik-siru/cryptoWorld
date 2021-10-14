9a01b27a6621b26948885af7c89b73b7ca10d701
karthik-siru <karthiksiruvalam@gmail.com>
2021-10-14 12:08:39 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Switch, Link, Route } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  NavBar,
+  Exchanges,
+  HomePage,
+  CryptoCurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./App.css";
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navBar">
+        <NavBar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <CryptoCurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer"></div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+} from "@ant-design/icons";
+import icon from "../images/cryptocurrency.png";
+
+const NavBar = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Cryptoverse</Link>
+        </Typography.Title>
+        {/* <Button className="menu-control-container"></Button> */}
+      </div>
+      <Menu theme="dark">
+        <Menu.Item Icon={HomeOutlined}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item Icon={FundOutlined}>
+          <Link to="/cryptocurrencies">CryptoCurrencies</Link>
+        </Menu.Item>
+        <Menu.Item Icon={MoneyCollectOutlined}>
+          <Link to="/exchanges">Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item Icon={BulbOutlined}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
+};
+
+export default NavBar;
